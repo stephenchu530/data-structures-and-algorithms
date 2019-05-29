bcff59bd70d220786e4ce5b969b5885cb68fcf7e
Stephen Chu <stephen.chu530@gmail.com>
2019-05-28 21:24:22 -0700

    feat: completed soln for code challenge 2

---
+++ CodeChallenges401/src/main/java/CodeChallenges401/ArrayShift.md
@@ -0,0 +1,12 @@
+# Challenge Summary
+<!-- Short summary or background information -->
+
+## Challenge Description
+<!-- Description of the challenge -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+<!-- Embedded whiteboard image -->
+![Whiteboard](/assets/ArrayShift.jpg)
--- CodeChallenges401/src/test/java/CodeChallenges401/ArrayReverseTest.java
+++ CodeChallenges401/src/test/java/CodeChallenges401/ArrayReverseTest.java
@@ -2,16 +2,14 @@ package CodeChallenges401;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import java.util.Arrays;
 
 public class ArrayReverseTest {
-    @Test public void testReverseArray() {
+    @Test
+    public void testReverseArray() {
         ArrayReverse classUnderTest = new ArrayReverse();
-        // create the array to test your code on later
         int[] startArr = new int[] {1,2,3,4,5,10};
         // call your reverseArray method and save the result in a variable
         int[] endArr = classUnderTest.reverseArray(startArr);
-        // print out the result, nicely
-        assertArrayEquals(new int[]{10, 5, 4, 3, 2, 1}, endArr);
+        assertArrayEquals(new int[]{10,5,4,3,2,1}, endArr);
     }
 }
