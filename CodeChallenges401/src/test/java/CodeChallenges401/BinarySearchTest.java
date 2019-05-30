66db14126db6a6576470e5ee4fa54effaec6df83
STEPHEN CHU <stephen.chu530@gmail.com>
2019-05-29 18:40:58 -0700

    test: added some tests

--- CodeChallenges401/src/test/java/CodeChallenges401/BinarySearchTest.java
+++ CodeChallenges401/src/test/java/CodeChallenges401/BinarySearchTest.java
@@ -29,5 +29,30 @@ public class BinarySearchTest {
         assertEquals(5, BinarySearch.binarySearch(testArray, 5));
         assertEquals(34, BinarySearch.binarySearch(testArray, 34));
         assertEquals(88, BinarySearch.binarySearch(testArray, 88));
+        assertEquals(-1, BinarySearch.binarySearch(testArray, 983));
+    }
+
+    @Test public void testBinarySearch10000() {
+        int[] testArray = new int[10000];
+
+        for (int i = 0; i < 10000; i++) {
+            testArray[i] = i;
+        }
+        assertEquals(23, BinarySearch.binarySearch(testArray, 23));
+        assertEquals(3794, BinarySearch.binarySearch(testArray, 3794));
+        assertEquals(8847, BinarySearch.binarySearch(testArray, 8847));
+        assertEquals(-1, BinarySearch.binarySearch(testArray, -1));
+    }
+
+    @Test public void testBinarySearch100000() {
+        int[] testArray = new int[100000];
+
+        for (int i = 0; i < 100000; i++) {
+            testArray[i] = i;
+        }
+        assertEquals(204, BinarySearch.binarySearch(testArray, 204));
+        assertEquals(59298, BinarySearch.binarySearch(testArray, 59298));
+        assertEquals(92858, BinarySearch.binarySearch(testArray, 92858));
+        assertEquals(-1, BinarySearch.binarySearch(testArray, 394823098));
     }
 }
