375a538cf20896ac304d779cf4479075d056d19a
Stephen Chu <stephen.chu530@gmail.com>
2019-06-11 21:58:21 -0700

    feat: basic setup for instance vars and constructor

--- fifoAnimalShelter/src/main/java/fifoAnimalShelter/AnimalShelter.java
+++ fifoAnimalShelter/src/main/java/fifoAnimalShelter/AnimalShelter.java
@@ -1,4 +1,11 @@
 package fifoAnimalShelter;
 
 public class AnimalShelter {
+    private Queue<String> catQ;
+    private Queue<String> dogQ;
+
+    public void AnimalShelter() {
+        this.catQ = new Queue<>();
+        this.dogQ = new Queue<>();
+    }
 }
